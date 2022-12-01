f69ca22bf742e23fda1327159283f967f24703aa
kevinpatel2323 <102168440+kevinpatel2323@users.noreply.github.com>
2022-12-01 17:14:26 +0530

    Add files via upload
    first

---
+++ sum_of_two_num.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class sum_of_two_num{
+    public static int addition(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("kevin");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int f = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int s = sc.nextInt();
+        int ans =  addition(f, s);
+        System.out.println(ans);
+
+       
+
+    }
+}
