1f2127928389ba680c32d44d4cc645770798b7ac
kevinpatel2323 <102168440+kevinpatel2323@users.noreply.github.com>
2022-12-03 17:44:39 +0000

    bubble sort optimized

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,9 +1,8 @@
 public class bubble_sort {
     //time-complexity = O(n^2)
     public static void sort(int a[]){
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length-1-i; j++) {
-                
+        for (int i = 0; i < a.length-2; i++) {
+            for (int j = 0; j < a.length-2-i; j++) {
                 if (a[j]>a[j+1]) {
                     int temp = a[j];
                     a[j] = a[j+1];
