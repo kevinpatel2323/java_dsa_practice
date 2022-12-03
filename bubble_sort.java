4a5da6bcd1c6fb97df737a6ca87b39217f9e6314
kevinpatel2323 <102168440+kevinpatel2323@users.noreply.github.com>
2022-12-03 17:56:34 +0000

    👍

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,13 +1,21 @@
 public class bubble_sort {
     //time-complexity = O(n^2)
+    
     public static void sort(int a[]){
+        
         for (int i = 0; i < a.length-1; i++) {
+            int swap = 0;
             for (int j = 0; j < a.length-1-i; j++) {
                 if (a[j]>a[j+1]) {
                     int temp = a[j];
                     a[j] = a[j+1];
                     a[j+1] =temp;
+                    swap++;
                 }
+                
+            }
+            if (swap==0) {
+                break;
             }
         }
         System.out.print("Sorted : ");
@@ -17,7 +25,7 @@ public class bubble_sort {
         
     }
     public static void main(String[] args) {
-        int[] a = {1,8,3,5,4,6,7,2,9};
+        int[] a = {1,2,3,4,5,6,7,8,9};
         System.out.print("Unsorted : ");
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]+" ");
